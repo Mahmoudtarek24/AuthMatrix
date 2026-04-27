3e8f6ee4694cbde485102ea48312f1a2826c8b54
Mahmoudtarek24 <Mahmoudtarek24@users.noreply.github.com>
2026-04-27 21:05:37 +0300

    update

--- AuthMatrix/WeatherForecast.cs
+++ AuthMatrix/WeatherForecast.cs
@@ -9,5 +9,10 @@ namespace AuthMatrix
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+    
+        public void Add(int x =10)
+        {
+
+        }
     }
 }
